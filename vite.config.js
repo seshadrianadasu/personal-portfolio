85d8facedb7c9956ef72f32ef434720f6dc155ad
Seshadri <seshadrianadasu@gmail.com>
2025-11-09 19:11:05 +0530

    correct base base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
-export default defineConfig({
-  base:'/Personal-Portfolio/',
-  plugins: [tailwindcss(),react()],
+export default defineConfig(() => {
+  const  active = process.env.VITE_ACTIVE_ENV || 'local'
+  const base = active === 'gh-pages' ? '/personal-portfolio/' : '/'
+    return {  
+          plugins: [tailwindcss(),react()],
+          base,
+      }
 })
